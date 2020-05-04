ecee01d74b7d12cf789be2d24ec518f568559e6a
GhostX <egamovkhurshed29@gmail.com>
2020-05-04 20:04:45 +0500

    Priject inited

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace NewMatrix
+{
+    class Program
+    {
+        static void Main()
+        {
+        }
+    }
+}
