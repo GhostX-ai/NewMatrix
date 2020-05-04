1eb7a8725853372dcad6e0f8f30f045489eee796
GhostX <egamovkhurshed29@gmail.com>
2020-05-04 20:05:23 +0500

    Important props has been added

--- Program.cs
+++ Program.cs
@@ -8,8 +8,37 @@ namespace NewMatrix
 {
     class Program
     {
+        static int Counter;
+        static Random RanPos = new Random();
+        static int FlowSpeed = 100;
+        static int FastFlow = FlowSpeed + 30;
+        static int TextSpeed = FastFlow + 50;
+
+        static ConsoleColor baseColor = ConsoleColor.DarkGreen;
+        static ConsoleColor greenColor = ConsoleColor.Green;
+        static ConsoleColor fadedColor = ConsoleColor.White;
+
+        static string endText = "There is a big power in MacBook Pro";
+
+        static char AssciCharacters
+        {
+            get
+            {
+                int t = RanPos.Next();
+                if (t <= 2)
+                    return (char)('0' + RanPos.Next(10));
+                else if (t <= 4)
+                    return (char)('a' + RanPos.Next(27));
+                else if (t <= 6)
+                    return (char)('A' + RanPos.Next(27));
+                else
+                    return (char)(RanPos.Next(32, 255));
+            }
+        }
         static void Main()
         {
-        }
+            }
+        
+        
     }
 }
